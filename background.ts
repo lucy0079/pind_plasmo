08f9c5e3f7c96963be2afffa364ddfa54d7ff471
sumin <dntn1052@gmail.com>
2025-08-04 15:23:55 +0900

    Revert "Merge api port update"

--- background.ts
+++ background.ts
@@ -1,9 +1,9 @@
 // background.ts
 
 // https://192.168.18.124:9000/ , https://172.20.10.4:9000/extract-ylocations
-const API_BASE_URL = "http://localhost:9001";
+const API_BASE_URL = "http://localhost:8000";
 // 개발 중인 pind-web-map의 주소
-const WEB_MAP_BASE_URL = "http://localhost:5174";
+const WEB_MAP_BASE_URL = "http://localhost:5173";
 
 async function processAndShowMap(youtubeUrl: string, jwtToken?: string, tokenType?: string) {
   console.log("백그라운드: processAndShowMap 함수 실행. URL:", youtubeUrl, "JWT:", jwtToken ? "있음" : "없음");
